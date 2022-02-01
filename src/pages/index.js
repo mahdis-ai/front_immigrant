aff751058210cff681e0abae95d816b2f84279d7
mahdis-ai <mahdisabedi127@gmail.com>
2022-02-01 11:39:53 +0330

    fixed 2nd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
-  
+import React ,{ useState ,useEffect } from 'react';
+import axios from "axios";
 const Home = () => {
+  const [start, setStart] = useState(null);
+  useEffect(() => {
+    getindex()
+      } ,[])
+function getindex() {
+  axios({
+      method: "GET",
+      url:"http://localhost:8000/",
+    }).then((response)=>{
+      const status = response.status
+      setStart(status);
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        }
+    })}
   return (
     <div
       style={{
@@ -10,7 +28,7 @@ const Home = () => {
         height: '100vh'
       }}
     >
-      <h1>Welcome to GeeksforGeeks</h1>
+      <h1>Welcome to this website</h1>
     </div>
   );
 };
